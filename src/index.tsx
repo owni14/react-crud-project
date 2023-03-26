c10e6405d495d79694dd8e72fe13eeceaa902ed2
owni14 <kmwoo514@gmail.com>
2023-03-26 12:44:56 +0900

    :seedling: initial: 초기 설정 완료

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global-style';
+import { theme } from '../src/styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/styles/global-style.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle, GlobalStyleComponent } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  html {
+    box-sizing: border-box;
+    font-size: 20px;
+    min-width: 320px;
+    height: 100%;
+  }
+
+  body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+  a { cursor: pointer; text-decoration: none; }
+`;
