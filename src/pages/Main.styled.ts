5ef77470e63a6cb8b4d7c09dbb3b6f016b55e91d
owni14 <kmwoo514@gmail.com>
2023-04-10 22:56:19 +0900

    :art: style: 메인화면 제작

--- src/pages/Main.styled.ts
+++ src/pages/Main.styled.ts
@@ -1,1 +1,25 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+export const MainText = styled.p`
+  font-size: 2.5rem;
+  font-weight: 700;
+`;
+
+export const SubBox = styled.div<{ theme: string }>`
+  color: ${({ theme }) => theme.colors.lightBlack};
+  text-align: center;
+`;
+
+export const SubText = styled.p`
+  font-size: 1.5rem;
+  margin: 0.2rem;
+`;
+
+export const LinkText = styled(Link)<{ theme: string }>`
+  color: ${({ theme }) => theme.colors.black};
+  font-weight: 700;
+  &:hover {
+    color: ${({ theme }) => theme.colors.gray};
+  }
+`;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,26 @@
+import { Link } from 'react-router-dom';
 import * as S from './Main.styled';
 
 const Main = () => {
-  return <>안녕하세요! Front End 개발자 Owni입니다.</>;
+  return (
+    <>
+      <S.MainText>안녕하세요! Front End 개발자 Owni입니다.</S.MainText>
+      <S.SubBox>
+        <S.SubText>
+          이 프로젝트는 React를 이용해 CRUD를 구현한 프로젝트입니다.🌟
+        </S.SubText>
+        <S.SubText>Firebase를 이용해 백엔드를 구성하였습니다.📁</S.SubText>
+        <S.SubText>자유롭게 글을 작성하고 서로의 의견을 나눠봐요!📝 </S.SubText>
+        <S.SubText>
+          저의
+          <S.LinkText to='https://github.com/owni14/react-crud-project'>
+            Github
+          </S.LinkText>
+          에서 코드를 확인하실수 있습니다.🔍
+        </S.SubText>
+      </S.SubBox>
+    </>
+  );
 };
 
 export default Main;
